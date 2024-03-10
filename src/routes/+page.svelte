f0f79230cab7e19cbb1513f6fac51c3ff77b82ca
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2024-03-10 10:20:58 +0700

    fix viewport height on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 	export let data;
 </script>
 
-<main class="flex min-h-screen flex-col items-center justify-center">
+<main class="flex min-h-[100dvh] flex-col items-center justify-center">
 	<Card class="w-[400px] max-w-full max-sm:border-none max-sm:bg-background max-sm:shadow-none">
 		<CardContent class="p-6">
 			<LoginForm data={data.form} />
