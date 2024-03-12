5c7a1a5f5d64c5bb3ed8e989c3360338f72e5643
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2024-03-12 14:03:50 +0700

    fix firebase env

--- src/lib/server/firebase/settings.ts
+++ src/lib/server/firebase/settings.ts
@@ -1,11 +1,13 @@
+import { VITE_FIREBASE_API_KEY, VITE_FIREBASE_AUTH_DOMAIN, VITE_FIREBASE_APP_ID, VITE_FIREBASE_USE_EMULATOR } from '$env/static/private';
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
   projectId: 'ria-busana',
   storageBucket: 'ria-busana.appspot.com',
   messagingSenderId: '1053993262752',
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  useEmulator: import.meta.env.VITE_FIREBASE_USE_EMULATOR === 'true'
+  appId: VITE_FIREBASE_APP_ID,
+  useEmulator: VITE_FIREBASE_USE_EMULATOR === 'true'
 };
 
 export { firebaseConfig };
