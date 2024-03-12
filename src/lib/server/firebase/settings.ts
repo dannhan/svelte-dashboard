a6d08c33a1dc3a7f4ee81cad3d6d161c12fa932b
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2024-03-12 19:35:39 +0700

    settings up vercel

--- src/lib/server/firebase/settings.ts
+++ src/lib/server/firebase/settings.ts
@@ -1,13 +1,14 @@
-// import { VITE_FIREBASE_API_KEY, VITE_FIREBASE_AUTH_DOMAIN, VITE_FIREBASE_APP_ID, VITE_FIREBASE_USE_EMULATOR } from '$env/static/private';
+import { VITE_FIREBASE_API_KEY, VITE_FIREBASE_AUTH_DOMAIN, VITE_FIREBASE_APP_ID, VITE_FIREBASE_USE_EMULATOR } from '$env/static/private';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBN3ItcUbNwvwNTUrhDNyVfdOnwbu1hU9o",
-  authDomain: "ria-busana.firebaseapp.com",
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  // todo
   projectId: 'ria-busana',
   storageBucket: 'ria-busana.appspot.com',
   messagingSenderId: '1053993262752',
-  appId: "1:1053993262752:web:e0f64d268e25eff00133c5",
-  useEmulator: false
+  appId: VITE_FIREBASE_APP_ID,
+  useEmulator: VITE_FIREBASE_USE_EMULATOR
 };
 
 export { firebaseConfig };
