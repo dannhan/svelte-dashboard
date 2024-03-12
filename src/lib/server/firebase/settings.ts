f910f6929d0cdcd97de4fa6b531117d1f80dd0c4
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2024-03-12 14:09:03 +0700

    almost ther

--- src/lib/server/firebase/settings.ts
+++ src/lib/server/firebase/settings.ts
@@ -1,13 +1,13 @@
-import { VITE_FIREBASE_API_KEY, VITE_FIREBASE_AUTH_DOMAIN, VITE_FIREBASE_APP_ID, VITE_FIREBASE_USE_EMULATOR } from '$env/static/private';
+// import { VITE_FIREBASE_API_KEY, VITE_FIREBASE_AUTH_DOMAIN, VITE_FIREBASE_APP_ID, VITE_FIREBASE_USE_EMULATOR } from '$env/static/private';
 
 const firebaseConfig = {
-  apiKey: VITE_FIREBASE_API_KEY,
-  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  apiKey: "AIzaSyBN3ItcUbNwvwNTUrhDNyVfdOnwbu1hU9o",
+  authDomain: "ria-busana.firebaseapp.com",
   projectId: 'ria-busana',
   storageBucket: 'ria-busana.appspot.com',
   messagingSenderId: '1053993262752',
-  appId: VITE_FIREBASE_APP_ID,
-  useEmulator: VITE_FIREBASE_USE_EMULATOR === 'true'
+  appId: "1:1053993262752:web:e0f64d268e25eff00133c5",
+  useEmulator: false
 };
 
 export { firebaseConfig };
